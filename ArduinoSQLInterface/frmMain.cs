91774f3f4d95a138d26e86a0b81cd4d5409e7f02
Atle-André Gundersen <atleag@outlook.com>
2016-10-06 11:50:40 +0200

    Can now save selected port, and configured TryParse for valid port numbers

--- ArduinoSQLInterface/frmMain.cs
+++ ArduinoSQLInterface/frmMain.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using System.Net.Sockets;
 
 
 namespace ArduinoSQLInterface
@@ -17,6 +18,7 @@ namespace ArduinoSQLInterface
 
         private string optionsTxtFile = ""; //Name of the options.txt file.
         private string[] optionsArray;
+        private int output = 1;
 
         public frmMain()
         {
@@ -39,11 +41,12 @@ namespace ArduinoSQLInterface
                 //Check if options file is created.
                 if (File.Exists(optionFile))
                 {
+                    ReadFromFile();
                     //Reads settings from file.
                     //Checks whether options have file has been initialized, if so, the program gets the path of the monitored folder.
-                    if (optionsArray[4] == "1")
+                    if (optionsArray[0] == "1")
                     {
-
+                        txtCurrPort.Text = optionsArray[1];
 
                     }
                 }
@@ -62,12 +65,34 @@ namespace ArduinoSQLInterface
 
         private void btnAssign_Click(object sender, EventArgs e)
         {
-            rtxtMessages.AppendText("\r\n Port {0} assigned");
+            string port = txtChaPort.Text;
+
+
+            //Check if input is within 65535
+            
+            Int32.TryParse(port, out output);
+            if (output >= 1 && output<= 65535)
+            {
+                string message = "";
+                message = "\r\n Port {0} assigned.";
+                rtxtMessages.AppendText(String.Format(message, output));
+                txtCurrPort.Text = Convert.ToString(output);
+
+                optionsArray[2] = Convert.ToString(output);
+                WriteToFile();
+            }
+            else
+            {
+                MessageBox.Show("Value can only be between 1 and 65535");
+            }
+
+
+
         }
 
         private void btnActivate_Click(object sender, EventArgs e)
         {
-            rtxtMessages.AppendText("\r\n Starting listening on port {0]...");
+            rtxtMessages.AppendText("\r\n Starting listening on port ...");
         }
 
         private void btnDeactivate_Click(object sender, EventArgs e)
@@ -80,6 +105,34 @@ namespace ArduinoSQLInterface
             //ScrollToCaret follows the next as it's appended to rtxtMessages
             //rtxtMessages.SelectionStart = rtxtMessages.Text.Length;
             rtxtMessages.ScrollToCaret();
+        }
+
+        private void UDPClient()
+        {
+            //UdpClient udpc = new UdpClient();
+            
+
+        }
+        private void ReadFromFile()
+        {
+            if (File.Exists(optionsTxtFile))
+            {
+                optionsArray = File.ReadAllLines(optionsTxtFile);
+            }
+        }
+        private void WriteToFile()
+        {
+            optionsArray[0] = "1";
+            optionsArray[1] = Convert.ToString(output);
+
+            // File.WriteAllLines(optionsFile, options);
+            using (StreamWriter file = new StreamWriter(optionsTxtFile))
+            {
+                foreach (string line in optionsArray)
+                {
+                    file.WriteLine(line);
+                }
+            }
         }
     }
 }
