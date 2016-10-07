be065d6be01089c4290a37024db27336e420ae75
Atle-André Gundersen <atleag@outlook.com>
2016-10-07 22:53:41 +0200

    Never Code when sleepy

--- ArduinoSQLInterface/frmMain.cs
+++ ArduinoSQLInterface/frmMain.cs
@@ -19,7 +19,9 @@ namespace ArduinoSQLInterface
 
         private string optionsTxtFile = ""; //Name of the options.txt file.
         private string[] optionsArray;
-        private int port = 1;
+        private int port;
+        private string udpIP;
+        private bool anySelected;
 
         public frmMain()
         {
@@ -47,7 +49,7 @@ namespace ArduinoSQLInterface
                     //Checks whether options have file has been initialized, if so, the program gets the path of the monitored folder.
                     if (optionsArray[0] == "1")
                     {
-                        txtCurrPort.Text = optionsArray[1];
+
 
                     }
                 }
@@ -56,6 +58,7 @@ namespace ArduinoSQLInterface
                     //if the options file doesnt exists, create file and create default settings.
                     Directory.CreateDirectory(pathWithName);
                     File.WriteAllText(optionFile, ("0\r\n" + "0\r\n" + "0\r\n" + "0\r\n" + "0\r\n"));
+                    rbtnAny.Enabled = true;
                 }
             }
             catch (Exception ex)
@@ -66,40 +69,36 @@ namespace ArduinoSQLInterface
 
         private void btnAssign_Click(object sender, EventArgs e)
         {
-            string input = txtChaPort.Text;
-
-
-            //Check if input is within 65535
-            
-            Int32.TryParse(input, out port);
-            if (port >= 1 && port <= 65535)
+            if (anySelected == false)
             {
-                string message = "";
-                message = "\r\n Port {0} assigned.";
-                rtxtMessages.AppendText(String.Format(message, port));
-                txtCurrPort.Text = Convert.ToString(port);
+                string input = txtChaPort.Text;
 
-                optionsArray[2] = Convert.ToString(port);
-                WriteToFile();
+                //Check if input is within 65535
+                Int32.TryParse(input, out port);
+                if (port >= 1 && port <= 65535)
+                {
+                    string message = "";
+                    message = "Port {0} assigned.\r\n";
+                    rtxtMessages.AppendText(String.Format(message, port));
+                    
+                }
+                else
+                {
+                    MessageBox.Show("Value can only be between 1 and 65535");
+                }
             }
-            else
-            {
-                MessageBox.Show("Value can only be between 1 and 65535");
-            }
-
-
-
         }
+
 
         private void btnActivate_Click(object sender, EventArgs e)
         {
-            rtxtMessages.AppendText("\r\n Starting listening on port ...");
+            rtxtMessages.AppendText("Starting listening on port...\r\n");
             ListenToPort();
         }
 
         private void btnDeactivate_Click(object sender, EventArgs e)
         {
-            rtxtMessages.AppendText("\r\n Deactivating port listening...");
+            rtxtMessages.AppendText("Deactivating port listening...\r\n");
         }
 
         private void rtxtMessages_TextChanged(object sender, EventArgs e)
@@ -112,20 +111,23 @@ namespace ArduinoSQLInterface
         private void UDPClient()
         {
             //UdpClient udpc = new UdpClient();
-            
-
         }
+
         private void ReadFromFile()
         {
             if (File.Exists(optionsTxtFile))
             {
                 optionsArray = File.ReadAllLines(optionsTxtFile);
+
             }
         }
+
         private void WriteToFile()
         {
             optionsArray[0] = "1";
             optionsArray[1] = Convert.ToString(port);
+            optionsArray[2] = udpIP;
+            optionsArray[3] = Convert.ToString(anySelected);
 
             // File.WriteAllLines(optionsFile, options);
             using (StreamWriter file = new StreamWriter(optionsTxtFile))
@@ -136,6 +138,7 @@ namespace ArduinoSQLInterface
                 }
             }
         }
+
         private void ListenToPort()
         {
             UdpClient udpClient = new UdpClient();
@@ -144,7 +147,7 @@ namespace ArduinoSQLInterface
 
             for (int i = 0; i < data.Length; i++)
             {
-                rtxtMessages.AppendText(String.Format("\r\n ", data[i]));
+                rtxtMessages.AppendText(String.Format(Convert.ToString(data[i]), "\r\n"));
             }
         }
         
@@ -156,7 +159,7 @@ namespace ArduinoSQLInterface
 
             for (int i = 0; i < data.Length; i++)
             {
-                rtxtMessages.AppendText(String.Format("\r\n ", data[i]));
+                rtxtMessages.AppendText(String.Format(Convert.ToString(data[i]), "\r\n"));
             }
         }
     
@@ -164,11 +167,13 @@ namespace ArduinoSQLInterface
         private void rbtnAny_CheckedChanged(object sender, EventArgs e)
         {
             txtIP.Enabled = false;
+            anySelected = false;
         }
 
         private void rbtnIP_CheckedChanged(object sender, EventArgs e)
         {
             txtIP.Enabled = true;
+            anySelected = true;
         }
 
         private void btnIP_Click(object sender, EventArgs e)
@@ -177,26 +182,32 @@ namespace ArduinoSQLInterface
             {
                 ValidateIPv4(txtIP.Text);
             }
+
         }
 
-        public void ValidateIPv4(string ipString)
+        private void ValidateIPv4(string ipString)
         {
+            
             if (ipString.Split(new char[] { '.' }, StringSplitOptions.RemoveEmptyEntries).Length == 4)
             {
                 IPAddress ipAddr;
                 if (IPAddress.TryParse(ipString, out ipAddr))
                 {
-                    MessageBox.Show("IP set to " + ipAddr.ToString());
-                }
-                else
-                {
-
+                    rtxtMessages.AppendText("IP set to " + ipAddr.ToString() + "\r\n");
+                    udpIP = ipString;
+                    WriteToFile();
                 }
             }
             else
             {
                 MessageBox.Show("Not an IP");
             }
+
+        }
+
+        private void InitializeGUI()
+        {
+
         }
     }
 }
