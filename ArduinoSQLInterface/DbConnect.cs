3b120be014affa23efa9833ffef7284aea0f0852
Atle <W840@W840-PC>
2016-10-25 14:48:37 +0200

    Updated DB connection

--- ArduinoSQLInterface/DbConnect.cs
+++ ArduinoSQLInterface/DbConnect.cs
@@ -1,30 +1,51 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Data;
 using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using System.ComponentModel;
 
-namespace ArduinoSQLInterface
+namespace ERP
 {
     public class DbConnect
     {
-
-        private string server;
-        private string database;
-        private string uid;
-        private string password;
         private SqlConnection connection;
+        public DataTable dt = new DataTable();
+        public string batchid = "";
+        public string conString = "";
+
+
 
         public DbConnect()
         {
-            server = "192.168.2.15";                                                    //Host
-            database = "IA5-5-16";                                                      //Database
-            uid = "sa";                                                                 //Username
-            password = "netlab_1";                                                      //Password
-            string connectionString;                                                    //Declare connectionString
-            connectionString = "SERVER=" + server + ";" + "DATABASE=" +                 //Format connectiongString
-            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
-            connection = new SqlConnection(connectionString);                           //Initialze connection with connectionString
+            //Initialze connection with connectionString
+            conString = "Data Source = 192.168.2.15\\SQLEXPRESS; Initial Catalog = IA5-5-16; User ID = sa; Password = " + "netlab_1";
+            connection = new SqlConnection(conString);
+            dt.Columns.Add("TypeOfCup", typeof(int));
+            dt.Columns.Add("OrderedWeight", typeof(int));
+            dt.Columns.Add("BatchID", typeof(int));
         }
+        public bool PingHost()
+        {
+            string nameOrAddress = "192.168.2.15";
+            bool pingable = false;
+            Ping pinger = new Ping();
+            try
+            {
+                PingReply reply = pinger.Send(nameOrAddress);
+                pingable = reply.Status == IPStatus.Success;
+            }
+            catch (PingException)
+            {
+                // Discard PingExceptions and return false;
+            }
+            return pingable;
+        }
+
         public bool OpenConnection()
         {
             try
@@ -64,6 +85,77 @@ namespace ArduinoSQLInterface
             return true;
         }
 
-        
+
+        public void CreateBatchOrder(int numberOfCups)
+        {
+            try
+            {
+                string query = "INSERT INTO BatchOrdre(NumberOfCups)VALUES(@numberOfCups); SELECT SCOPE_IDENTITY();";
+                if (OpenConnection())
+                {
+                    using (SqlCommand cmd = new SqlCommand(query, connection))
+                    {
+                        cmd.Parameters.AddWithValue("@numberOfCups", numberOfCups);
+                        batchid = cmd.ExecuteScalar().ToString();
+                        CloseConnection();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public void InsertOrderIntoDataTable(int numOfCups, int typeOfCup, int fillLevel)
+        {
+            try
+            {
+                for (int i = 0; i < numOfCups; i++)
+                {
+                    string query = "INSERT INTO CupOrdre(TypeOfCup, OrderedWeight, BatchID)VALUES(@typeOfCup, @orderedWeight, @batchID);";
+                    if (OpenConnection())
+                    {
+                        using (SqlCommand cmd = new SqlCommand(query, connection))
+                        {
+                            cmd.Parameters.AddWithValue("@typeOfCup", typeOfCup);
+                            cmd.Parameters.AddWithValue("@orderedWeight", fillLevel);
+                            cmd.Parameters.AddWithValue("@batchID", batchid);
+                            cmd.ExecuteNonQuery();
+                            CloseConnection();
+                        }
+                    }
+
+                }
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public void GetBatchOrder()
+        {
+            try
+            {
+                string query = "SELECT * FROM BatchOrdre WHERE BatchID = @batchID";
+                if (OpenConnection())
+                {
+                    using (SqlCommand cmd = new SqlCommand(query, connection))
+                    {
+                        cmd.Parameters.AddWithValue("@batchID", batchid);
+                        dt.Load(cmd.ExecuteReader());
+                        CloseConnection();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
     }
 }
+
